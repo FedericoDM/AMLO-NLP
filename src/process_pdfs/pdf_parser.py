7608f633c078d1fd8b0a106325a25990e92eb766
Federico Domínguez Molina <fd.molina@outlook.com>
2024-02-29 14:24:59 -0600

    Almost done with class

--- src/process_pdfs/pdf_parser.py
+++ src/process_pdfs/pdf_parser.py
@@ -27,7 +27,7 @@ class PDFParser:
         self.all_files_gob = os.listdir(self.HOMICIDIOS_GOB_PATH)
         self.all_files_abiertos = os.listdir(self.HOMICIDIOS_ABIERTOS_PATH)
 
-    def extract_text_from_pdf(self, pdf_file_path):
+    def extract_homicides_from_pdf(self, pdf_file_path):
         """
         This function extracts the text from a PDF file
         """
@@ -57,27 +57,60 @@ class PDFParser:
         """
         Get the total number of homicides from the government's PDFs
         """
+        self.total_homicides_gov = []
+
+        for file_gob in self.all_files_gob:
+            if file_gob.endswith(".pdf"):
+                text_gob = self.extract_homicides_from_pdf(
+                    os.path.join(self.HOMICIDIOS_GOB_PATH, file_gob)
+                )
+
+                self.total_homicides_gov.append(text_gob)
+
+            else:
+                self.total_homicides_gov.append(None)
+
+    def get_total_homicides_abierto(self):
+        """
+        Get the total number of homicides from the abierto's PDFs
+        """
+        self.total_homicides_abierto = []
+
+        for file_abierto in self.all_files_abiertos:
+            if file_abierto.endswith(".pdf"):
+                text_abierto = self.extract_homicides_from_pdf(
+                    os.path.join(self.HOMICIDIOS_ABIERTOS_PATH, file_abierto)
+                )
+
+                self.total_homicides_abierto.append(text_abierto)
+
+            else:
+                self.total_homicides_abierto.append(None)
+
+    def create_table(self):
+        """
+        Create a table with the total homicides
+        """
+
+        # Extract dates from files
+        dates = [re.findall(r"\d+", homicide)[0] for homicide in self.all_files_gob]
+
+        # Create the table
+        table = pd.DataFrame(
+            {
+                "dates": dates,
+                "homicidios_fuentes_gobierno": self.total_homicides_gov,
+                "homicidios_fuentes_abiertas": self.total_homicides_abierto,
+            }
+        )
+
+        return table
 
 
 if __name__ == "__main__":
     # Get the list of files
-    HOMICIDIOS_GOB_PATH = os.path.join(DATA_PATH, "homicidios_gobierno/")
-    HOMICIDIOS_ABIERTOS_PATH = os.path.join(DATA_PATH, "homicidios_abierto/")
-    all_files_gob = os.listdir(HOMICIDIOS_GOB_PATH)
-    all_files_abiertos = os.listdir(HOMICIDIOS_ABIERTOS_PATH)
-
-    # Extract the text from each PDF
-    for file_gob, file_abierto in zip(all_files_gob[:3], all_files_abiertos[:3]):
-        if file_gob.endswith(".pdf"):
-            text_gob = extract_text_from_pdf(
-                os.path.join(HOMICIDIOS_GOB_PATH, file_gob)
-            )
-            print(text_gob)
-
-        print("========================================")
-
-        if file_abierto.endswith(".pdf"):
-            text_abierto = extract_text_from_pdf(
-                os.path.join(HOMICIDIOS_ABIERTOS_PATH, file_abierto)
-            )
-            print(text_abierto)
+    pdf_parser = PDFParser()
+    print("Processing total homicides from government's PDFs")
+    pdf_parser.get_total_homicides_government()
+    print("Processing total homicides from open sources PDFs")
+    pdf_parser.get_total_homicides_abierto()
