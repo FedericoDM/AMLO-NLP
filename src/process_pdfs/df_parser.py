6e15c94e9a824d21eaf2462c7e7a629602ea02b3
Federico Domínguez Molina <fd.molina@outlook.com>
2024-03-01 12:51:39 -0600

    Finished merging datasets

--- src/process_pdfs/df_parser.py
+++ src/process_pdfs/df_parser.py
@@ -5,6 +5,7 @@ Merges the AMLO and Homicides datasets
 import os
 import re
 import pandas as pd
+from datetime import datetime
 
 DATA_PATH = "C:/Users/fdmol/Desktop/AMLO-NLP/src/data/"
 
@@ -19,17 +20,47 @@ class DFParser:
         )
         self.amlo_df = pd.read_csv(os.path.join(DATA_PATH, self.AMLO_SCORE_FILENAME))
 
-    def process_df(self):
-        pass
+    def join_dfs(self):
+        """
+        This function joins the two dataframes
+        """
+
+        self.homicides_df["dates"] = self.homicides_df["dates"].astype(str)
+
+        print(self.homicides_df.head())
+        print(self.amlo_df.head())
+
+        self.amlo_df["id"] = self.amlo_df["id"].astype(str)
+
+        self.amlo_df["date"] = self.amlo_df["id"].apply(
+            lambda x: datetime.strptime(x, "%Y%m%d")
+        )
+
+        # Back to string, with %d%m%Y format
+
+        self.amlo_df["date"] = self.amlo_df["date"].apply(
+            lambda x: datetime.strftime(x, "%d%m%Y")
+        )
+
+        # Merge the dataframes
+
+        self.df = pd.merge(
+            self.amlo_df,
+            self.homicides_df,
+            left_on="date",
+            right_on="dates",
+            how="left",
+        )
+
+        # Drop the dates column
+        self.df.drop(columns=["dates"], inplace=True)
 
 
 if __name__ == "__main__":
 
     parser = DFParser()
-    parser.process_df()
+    parser.join_dfs()
 
-    parser.df.to_csv(
-        os.path.join(DATA_PATH, "homicidios_totales_clean.csv"), index=False
-    )
+    parser.df.to_csv(os.path.join(DATA_PATH, "complete_datasets.csv"), index=False)
 
     print(parser.df.head())
