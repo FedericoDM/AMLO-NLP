6948769ddfe14aeb64be99f800acf2c6c7192916
Federico Domínguez Molina <fd.molina@outlook.com>
2024-03-09 00:47:40 -0600

    Trying

--- src/nlp_analyzer/neural_network.py
+++ src/nlp_analyzer/neural_network.py
@@ -3,19 +3,17 @@ This script is in charge of creating the Neural Network to predict
 the aggressivity of the president's dialogues
 """
 
-from collections import Counter
-
 import numpy as np
 import pandas as pd
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-from torch.utils.data import DataLoader, Dataset
-from tqdm import tqdm
 
 # Minmaxscaler
 from sklearn.preprocessing import MinMaxScaler
+
+from utils.constants import DATA_PATH
 
 
 # Classes and functions
@@ -38,34 +36,81 @@ class NeuralTrainer:
     def __init__(
         self,
         num_epochs,
+        vocab,
+        embedding_dim,
+        train_loader,
+        unseen_loader,
+        training_df,
+        unseen_df,
     ) -> None:
         self.num_epochs = num_epochs
+        self.vocab = vocab
+        self.embedding_dim = embedding_dim
+        self.train_loader = train_loader
+        self.unseen_loader = unseen_loader
+        self.training_df = training_df
+        self.unseen_df = unseen_df
 
-    def train_model(self, vocab, train_loader):
+    def train_model(self):
         """
-        This function wraps the entire training process: data loading, model creation,
+        This function wraps the entire training process: data loading,
+        model creation and training.
         """
-        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-        model = NeuralRegressor(vocab_size=len(vocab) + 1, embedding_dim=100).to(device)
+        self.model = NeuralRegressor(
+            vocab_size=len(self.vocab) + 1, embedding_dim=self.embedding_dim
+        ).to(self.device)
         criterion = nn.MSELoss()
-        optimizer = optim.Adam(model.parameters(), lr=0.001)
+        optimizer = optim.Adam(self.model.parameters(), lr=0.001)
 
         for epoch in range(self.num_epochs):
-            model.train()
+            self.model.train()
 
-            for batch in train_loader:
+            for batch in self.train_loader:
 
-                inputs, targets = (
-                    batch  # Here, batch directly unpacks into inputs and targets
-                )
-
-                inputs, targets = inputs.to(device), targets.to(device)
+                inputs, targets = batch
+                inputs, targets = inputs.to(self.device), targets.to(self.device)
 
                 optimizer.zero_grad()
-                outputs = model(inputs).squeeze(1)
+                outputs = self.model(inputs).squeeze(1)
                 loss = criterion(outputs, targets)
                 loss.backward()
                 optimizer.step()
 
-            print(f"Epoch {epoch+1}/{num_epochs}, Loss: {loss.item():.4f}")
+            print(f"Epoch {epoch+1}/{self.num_epochs}, Loss: {loss.item():.4f}")
+
+    def predict_unseen_data(self):
+        """
+        Predicts the labels for the unseen data.
+        """
+
+        self.model.to(self.device)
+        self.model.eval()
+        with torch.no_grad():
+            self.predictions = []
+            for inputs in self.unseen_loader:  # No targets here
+                inputs = inputs.to(
+                    self.device
+                )  # Ensure inputs are on the correct device
+                outputs = self.model(inputs).squeeze(1)
+                self.predictions.extend(outputs.cpu().numpy())
+
+    def scale_data(self):
+        """
+        Scales the predictions and saves the DataFrame
+        """
+        self.unseen_df["score"] = self.predictions
+
+        self.nnet_agressivity_scores = pd.concat(
+            [self.training_df, self.unseen_df], axis=0
+        )
+
+        scaler = MinMaxScaler()
+        # Normalize the score
+        scores = np.array(self.nnet_agressivity_scores["score"]).reshape(-1, 1)
+        scaler.fit(scores)
+        self.nnet_agressivity_scores["score"] = scaler.transform(scores)
+        self.nnet_agressivity_scores.to_csv(
+            f"{DATA_PATH}nnet_agressivity_scores.csv", index=False
+        )
